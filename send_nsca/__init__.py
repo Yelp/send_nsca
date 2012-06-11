4b1e2fed95e38a0c75e0854df08eb39ca593baab
James Brown <jbrown@yelp.com>
2012-06-11 13:58:52 -0700

    change import path

--- send_nsca/__init__.py
+++ send_nsca/__init__.py
@@ -1,4 +1,4 @@
-from .import (States, STATE_OK, STATE_WARNING, STATE_CRITICAL, STATE_UNKNOWN)
+from .nagios import (States, STATE_OK, STATE_WARNING, STATE_CRITICAL, STATE_UNKNOWN)
 from .nsca import NscaSender, log
 
 # make pyflakes happy
